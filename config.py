1f1a8f6ee31a0cf8b05791835d7d2098892692c0
dheeraj742 <134304251+dheeraj742@users.noreply.github.com>
2023-12-01 21:54:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
-customuser = "intellipaat"
-custompass = "intellipaat123"
+customuser = "Dheerajvarma"
+custompass = "Dheeraj123"
 customdb = "employee"
 custombucket = "addemployee"
 customregion = "us-east-2"
